1b805c08e6fcd6a96eb7c10cd1d5f476e41516f9
Ali Hassaine <ali.hs@outlook.fr>
2022-12-08 20:32:42 +0000

    Add stepper itemsc

--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import desktopSidebar from '../assets/images/bg-sidebar-desktop.svg';
 import { ReactComponent as DesktopSidebar } from '../assets/images/bg-sidebar-desktop.svg'
+import StepperItem from './StepperItem';
+
+
+const STEPS = [
+  {
+    stepId: 1,
+    stepLabel: 'YOUR INFO'
+  },
+  {
+    stepId: 2,
+    stepLabel: 'SELECT PLAN'
+  },
+  {
+    stepId: 3,
+    stepLabel: 'ADD-ONS'
+  },
+  {
+    stepId: 4,
+    stepLabel: 'SUMMARY'
+  }
+]
+
 function Stepper() {
   return <>
     <div style={{ position: 'relative'}}>
       <DesktopSidebar />
-      <div className='absolute inset-0'>Hello there</div>
+      <div className='absolute inset-0'>
+        <div id="steps" className='mt-8 w-2/3 mx-auto flex flex-col gap-8'>
+          {STEPS.map((step) => {
+            return <StepperItem key={step.stepId} stepNumber={step.stepId} stepLabel={step.stepLabel} active={step.stepId === 1}/>
+          })}
+        </div>
+      </div>
     </div>
   </>;
 }
---
+++ src/components/StepperItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const ACTIVE_CLASSES = 'bg-primary-light text-gray-900'
+const INACTIVE_CLASSES = 'bg-transparent border border-slate-100 text-white'
+function StepperItem({stepNumber, stepLabel, active}) {
+  return (
+    <div className='flex gap-5 items-center'>
+        <button className={`${active ? ACTIVE_CLASSES : INACTIVE_CLASSES} transition-colors font-bold rounded-full w-10 h-10 flex-shrink-0 hover:text-gray-900 hover:bg-primary-light`}>{stepNumber}</button>
+        <div className='flex flex-col'>
+        <span className='text-neutral-lightgray'>STEP {stepNumber}</span>
+        <span className='text-neutral-magnolia font-bold'>{stepLabel}</span>
+        </div>
+    </div>
+  )
+}
+
+export default StepperItem
