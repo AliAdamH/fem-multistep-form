e5bf43b804e37af4323513a453bcb1c60fb8c80b
Ali Hassaine <ali.hs@outlook.fr>
2022-12-08 21:27:28 +0000

    Add initial step change

--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import desktopSidebar from '../assets/images/bg-sidebar-desktop.svg';
 import { ReactComponent as DesktopSidebar } from '../assets/images/bg-sidebar-desktop.svg'
 import StepperItem from './StepperItem';
@@ -24,13 +24,19 @@ const STEPS = [
 ]
 
 function Stepper() {
+  const [activeStep, setActiveStep ] = useState(STEPS[0].stepId)
+
+  const handleStepActivation = (step) => {
+    setActiveStep(step)
+  }
+
   return <>
     <div style={{ position: 'relative'}}>
       <DesktopSidebar />
       <div className='absolute inset-0'>
         <div id="steps" className='mt-8 w-2/3 mx-auto flex flex-col gap-8'>
           {STEPS.map((step) => {
-            return <StepperItem key={step.stepId} stepNumber={step.stepId} stepLabel={step.stepLabel} active={step.stepId === 1}/>
+            return <StepperItem activate={handleStepActivation} key={step.stepId} stepNumber={step.stepId} stepLabel={step.stepLabel} active={step.stepId === activeStep}/>
           })}
         </div>
       </div>
--- src/components/StepperItem.jsx
+++ src/components/StepperItem.jsx
@@ -3,10 +3,15 @@ import React from 'react'
 
 const ACTIVE_CLASSES = 'bg-primary-light text-gray-900'
 const INACTIVE_CLASSES = 'bg-transparent border border-slate-100 text-white'
-function StepperItem({stepNumber, stepLabel, active}) {
+function StepperItem({stepNumber, stepLabel, active, activate}) {
+
+  const activeSelf = () => {
+    activate(stepNumber)
+  }
+
   return (
     <div className='flex gap-5 items-center'>
-        <button className={`${active ? ACTIVE_CLASSES : INACTIVE_CLASSES} transition-colors font-bold rounded-full w-10 h-10 flex-shrink-0 hover:text-gray-900 hover:bg-primary-light`}>{stepNumber}</button>
+        <button onClick={activeSelf} className={`${active ? ACTIVE_CLASSES : INACTIVE_CLASSES} transition-colors font-bold rounded-full w-10 h-10 flex-shrink-0 hover:text-gray-900 hover:bg-primary-light`}>{stepNumber}</button>
         <div className='flex flex-col'>
         <span className='text-neutral-lightgray'>STEP {stepNumber}</span>
         <span className='text-neutral-magnolia font-bold'>{stepLabel}</span>
