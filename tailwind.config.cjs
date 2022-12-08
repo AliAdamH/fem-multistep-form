84aadf310e39362edfeff448f3c759a683170721
Ali Hassaine <ali.hs@outlook.fr>
2022-12-08 19:48:07 +0000

    Add custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          marine: 'hsl(213, 96%, 18%)',
+          purplish: 'hsl(243, 100%, 62%)',
+          pastel: 'hsl(228, 100%, 84%)',
+          light: 'hsl(206, 94%, 87%)',
+          strawberry: 'hsl(354, 84%, 57%)',
+        },
+        neutral: {
+          coolgray: 'hsl(231, 11%, 63%)',
+          lightgray: 'hsl(229, 24%, 87%)',
+          magnolia: 'hsl(217, 100%, 97%)',
+          alabaster: 'hsl(231, 100%, 99%)',
+          white: 'hsl(0, 0%, 100%)',
+        },
+      },
+    },
   },
   plugins: [],
 };
