8aa66d34800683fb519de0e3f1a1b42006f6b50c
Ali Hassaine <ali.hs@outlook.fr>
2022-12-09 14:09:04 +0000

    Replace steps array to lookup table
    In order to make it easier to change steps,
    child components have the ability to change
    the active step's id, therefore it is necessary
    for the parent component to use this id efficiently
    which is why using a lookup table is the better choice here.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FormContent from './FormContent';
 import Stepper from './Stepper';
 import AddOns from './AddOns';
@@ -6,41 +6,41 @@ import PlanSelection from './PlanSelection';
 import Summary from './Summary';
 import PersonalInfo from './PersonalInfo';
 
-const STEPS = [
-  {
+const STEPS = {
+  1: {
     stepId: 1,
     stepLabel: 'YOUR INFO',
     view: PersonalInfo
   },
-  {
+  2: {
     stepId: 2,
     stepLabel: 'SELECT PLAN',
     view: PlanSelection
   },
-  {
+  3:{
     stepId: 3,
     stepLabel: 'ADD-ONS',
     view: AddOns
   },
-  {
+  4: {
     stepId: 4,
     stepLabel: 'SUMMARY',
     view: Summary
   }
-]
+}
 
 function Form() {
-  const [activeStep, setActiveStep] = useState(null)
+  const [activeStep, setActiveStep] = useState(STEPS[1])
 
-  const handleStepChange = (step) => {
-    setActiveStep(step)
+  const handleStepChange = (id) => {
+    setActiveStep(STEPS[id])
   }
-
+  
   return (
     <>
       <div className="max-w-4xl bg-gray-500 p-3 rounded-lg flex gap-3">
         <Stepper steps={STEPS} forwardActiveStep={handleStepChange} />
-        <FormContent activeStep={activeStep} />
+        <FormContent activeStep={activeStep} changeStep={handleStepChange} />
       </div>
     </>
   );
