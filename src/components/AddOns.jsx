72df86cf4ab343605e41aed0b3106baeba45653b
Ali Hassaine <ali.hs@outlook.fr>
2022-12-11 13:32:14 +0000

    Add addons state awareness

--- src/components/AddOns.jsx
+++ src/components/AddOns.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-function AddOns() {
+function AddOns({ onlineServiceAddon, largerStorageAddon, customizableProfileAddon, changeData }) {
+
+  const renderClassesIfChecked = (addOn) => {
+    return addOn ? 'bg-neutral-magnolia border-primary-purplish' : '';
+  }
+
   return (
     <>
     <div id="header" className='mb-8'>
@@ -8,28 +13,28 @@ function AddOns() {
       <p className='text-neutral-coolgray'>Please provide your name, email and phone number</p>
     </div>
     <div id="fields" className='flex flex-col gap-4'>
-      <div className='p-4 border border-neutral-lightgray rounded-lg flex gap-10 items-center hover:border-primary-purplish'>
-        <input type="checkbox" name="onlineService" id="onlineService" value={'true'} className='w-4 h-4' />
-        <label htmlFor="onlineService" className='text-primary-marine font-semibold'>
+      <div className={`${renderClassesIfChecked(onlineServiceAddon)} p-4 border border-neutral-lightgray rounded-lg flex gap-10 items-center hover:border-primary-purplish`}>
+        <input onChange={(e) => changeData({[e.target.name]: !onlineServiceAddon})} type="checkbox" name="onlineServiceAddon" id="onlineServiceAddon" checked={onlineServiceAddon} className='w-4 h-4' />
+        <label htmlFor="onlineServiceAddon" className='text-primary-marine font-semibold'>
           Online Service <br />
           <span className='text-sm text-neutral-coolgray font-normal'>Access to mulitplayer games</span>
         </label>
         <span className='inline-block ml-auto text-primary-purplish text-sm font-semibold'>+$1/mo</span>
       </div>
 
-      <div className='p-4 border border-neutral-lightgray rounded-lg flex gap-10 items-center hover:border-primary-purplish'>
-        <input type="checkbox" name="largerStorage" id="largerStorage" value={'true'}  className='w-4 h-4'/>
-        <label htmlFor="largerStorage" className='text-primary-marine font-semibold'>
+      <div className={`${renderClassesIfChecked(largerStorageAddon)} p-4 border border-neutral-lightgray rounded-lg flex gap-10 items-center hover:border-primary-purplish`}>
+        <input onChange={(e) => changeData({[e.target.name]: !largerStorageAddon})} type="checkbox" name="largerStorageAddon" id="largerStorageAddon" checked={largerStorageAddon}  className='w-4 h-4'/>
+        <label htmlFor="largerStorageAddon" className='text-primary-marine font-semibold'>
           Larger Storage <br />
           <span className='text-sm text-neutral-coolgray font-normal'>Extra 1TB of cloud save</span>
         </label>
         <span className='inline-block ml-auto text-primary-purplish text-sm font-semibold'>+$2/mo</span>
       </div>
 
-      <div className='p-4 border border-neutral-lightgray rounded-lg flex gap-10 items-center hover:border-primary-purplish'>
-        <input type="checkbox" name="customiZableProfile" id="customiZableProfile" value={'true'} className='w-4 h-4' />
+      <div className={`${renderClassesIfChecked(customizableProfileAddon)} p-4 border border-neutral-lightgray rounded-lg flex gap-10 items-center hover:border-primary-purplish`}>
+        <input onChange={(e) => changeData({[e.target.name]: !customizableProfileAddon})}  type="checkbox" name="customizableProfileAddon" id="customizableProfileAddon" checked={customizableProfileAddon} className='w-4 h-4' />
         
-        <label htmlFor="customiZableProfile" className='text-primary-marine font-semibold'>
+        <label htmlFor="customizableProfileAddon" className='text-primary-marine font-semibold'>
           Customizable Profile <br />
           <span className='text-sm text-neutral-coolgray font-normal'>Custom theme on your profile</span>
         </label>
