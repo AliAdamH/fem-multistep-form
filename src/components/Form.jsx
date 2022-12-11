5db68ee9c3722a387e0768fdbf3544d267466c06
Ali Hassaine <ali.hs@outlook.fr>
2022-12-11 16:02:56 +0000

    Add default values
    Default values are better than just hardcoding
    the values inside components. This also has the
    benefit of adding the ability to iterate through
    the default values and generate the corresponding markup,
    as well as an easier way to calculate the total value of the form.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,12 +7,51 @@ import Summary from './Summary';
 import PersonalInfo from './PersonalInfo';
 
 
+const defaultValues = {
+  plans: {
+    arcade: {
+      monthly: 9,
+      yearly: 90
+    },
+    advanced: {
+      monthly: 12,
+      yearly: 120
+    },
+    pro: {
+      monthly: 15,
+      yearly: 150
+    }
+  },
+  addOns: {
+    onlineService: {
+      monthly: 1,
+      yearly: 10
+    },
+    largerStorage: {
+      monthly: 2,
+      yearly: 20
+    },
+    customizableProfile: {
+      monthly: 2,
+      yearly: 20
+    }
+  }
+}
+
+const betterInitialState = {
+  name: '',
+  email: '',
+  phone: '',
+  selectedPlan: defaultValues.plans['arcade'],
+  planFrequency: 'monthly',
+  selectedAddons: {}
+}
 
 const initialState = {
   name: '',
   email: '',
   phone: '',
-  planType: '',
+  planType: 'arcade',
   planAmount: '',
   planFrequency: 'monthly',
   onlineServiceAddon: false,
