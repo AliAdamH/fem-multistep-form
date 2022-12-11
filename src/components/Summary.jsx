5d04b980dcf3bf8f0d84e3f65c3333740bc0a9a7
Ali Hassaine <ali.hs@outlook.fr>
2022-12-11 19:11:50 +0000

    Add total calculation

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 
-function Summary({ selectedPlan, planFrequency }) {
+function Summary({ selectedPlan, planFrequency, selectedAddons }) {
+
+  const periodFormatter = planFrequency === 'monthly' ? 'month' : 'year';
+
+  const TotalPrice = () => {
+    return [selectedPlan, ...Object.values(selectedAddons)].reduce((accumulator, currentObject) => {
+      return accumulator += currentObject[planFrequency]
+    }, 0)
+  }
+
+  const displayPriceHelper = (pricedItem) => {
+    return planFrequency === 'monthly' ? `$${pricedItem.monthly}/mo` : `$${pricedItem.yearly}/yr`;
+  }
+
   return (
     <>
     <div id="header" className='mb-8'>
-      <h2 className='tex  t-3xl font-bold text-primary-marine'>Finishing up</h2>
+      <h2 className='text-3xl font-bold text-primary-marine'>Finishing up</h2>
       <p className='text-neutral-coolgray'>Double-check everything before confirming</p>
     </div>
      <div data-summary className='bg-neutral-magnolia p-5 rounded space-y-4'>
@@ -13,25 +26,24 @@ function Summary({ selectedPlan, planFrequency }) {
             <h3 className='font-bold text-primary-marine capitalize'>{selectedPlan.label} ({planFrequency})</h3>
             <a href="#" className='text-sm text-neutral-coolgray underline'>Change</a>
           </div>
-          <span className='font-bold text-primary-marine text-sm ml-auto'>$9/mo</span>
+          <span className='font-bold text-primary-marine text-sm ml-auto'>{displayPriceHelper(selectedPlan)}</span>
         </div>
         <div data-addon-summary className='flex flex-col gap-4'>
-            {/* Make this dynamic ofcourse. */}
-            <div className='flex justify-between items-center text-sm'>
-              <span className='text-neutral-coolgray'> Online Service </span>
-              <span className='text-primary-marine'> +$1/mo</span>
-            </div>
-
-            <div className='flex justify-between items-center text-sm'>
-              <span className='text-neutral-coolgray'>Larger storage  </span>
-              <span className='text-primary-marine'> +$2/mo</span>
-            </div>
-
+            {
+              Object.keys(selectedAddons).map((addOn) => {
+                return (
+                  <div  key={addOn} className='flex justify-between items-center text-sm'>
+                    <span className='text-neutral-coolgray'> {selectedAddons[addOn].label}</span>
+                    <span className='text-primary-marine'> +{displayPriceHelper(selectedAddons[addOn])}</span>
+                  </div>
+                )
+              })
+            }
         </div>
      </div>
-    <div data-total className='flex justify-between items-center mt-8 px-6'>
-      <span className='text-sm text-neutral-coolgray'>Total{'(per Month)'}</span>
-      <span className='text-lg text-primary-purplish font-bold'>+$12/mo</span>
+    <div data-total className='flex justify-between items-center mt-8 px-6 capitalize'>
+      <span className='text-sm text-neutral-coolgray'>Total per {periodFormatter}</span>
+      <span className='text-lg text-primary-purplish font-bold '>${TotalPrice()}/{periodFormatter}</span>
     </div>
     </>
   )
