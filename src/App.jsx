b932e19d0f6e715bf47cc820eda44e87fdb9bcff
Ali Hassaine <ali.hs@outlook.fr>
2022-12-18 21:13:11 +0000

    Remove unused state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from 'react';
 import Form from './components/Form';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="bg-neutral-magnolia flex md:justify-center md:items-center h-screen">
       <Form />
